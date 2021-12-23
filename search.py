836d62779ff82dc9e4a434ef242b60e705f8a490
song1232 <92849386+song1232@users.noreply.github.com>
2021-12-23 11:56:53 +0800

    Update search.py

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ def init(params: dict):
 def run(data: dict, func=None) -> bool:
     if data["objectPath"].startswith('207'):
         return True
-    if data["fileSize"] > 2*1024:
+    if data["fileSize"] > 2*1024*1024:
         return True
     return False
 
